f9f11f53c8919a3d1d93583e58fcec8eb4c1d093
Egor Koldasov <koldasov3@gmail.com>
2021-12-08 19:06:10 +0300

    update config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,14 +1,14 @@
 import { isServerEnvironment } from "./environment";
+import { booleanVar, numberVar, stringVar } from "./envVar";
 
-const apiServerHost = process.env.API_HOST;
-const apilClientHost = process.env.NEXT_PUBLIC_API_HOST;
+const apiServerHost = stringVar('API_HOST');
+const apilClientHost = stringVar('NEXT_PUBLIC_API_HOST');
 const isServer = isServerEnvironment();
-const envToString = (env: string | undefined) => env ? env : ''
 
 const config = {
-  apiSsl: process.env.NEXT_PUBLIC_API_SSL === 'FALSE' ? false : true,
+  apiSsl: booleanVar('NEXT_PUBLIC_API_SSL', true),
   apiHost: isServer ? apiServerHost : apilClientHost,
-  apiPort: parseInt(String(process.env.NEXT_PUBLIC_API_PORT)) || 443,
-  logLvl: envToString(process.env.NEXT_PUBLIC_LOG_LVL) || 'ERROR',
+  apiPort: numberVar('NEXT_PUBLIC_API_PORT', 443),
+  logLvl: stringVar('NEXT_PUBLIC_LOG_LVL', 'ERROR'),
 };
 export default config;
---
+++ src/lib/envVar.ts
@@ -0,0 +1,43 @@
+
+export const requireVar = (name: string): string => {
+  const env = process.env[name];
+  if (env === undefined) throw new Error(`Environment variable "${name}" is not set`);
+  return env;
+}
+export const stringVar = (name: string, defaultVal?: string): string => {
+  if (defaultVal === undefined) {
+    return requireVar(name);
+  }
+  const env = process.env[name];
+  return env !== undefined ? env : defaultVal;
+}
+
+const stringVarToNumber = <DefaultVal>(envVar: string, defaultVal: DefaultVal): number | DefaultVal => {
+  const falseNumber = parseInt(envVar);
+  if (!isFinite(falseNumber)) {
+    return defaultVal
+  };
+  return falseNumber;
+}
+
+export const numberVar = (name: string, defaultVal?: number): number => {
+  if (defaultVal === undefined) {
+    const number = stringVarToNumber(requireVar(name), undefined);
+    if (number === undefined) throw new Error(`Environment variable ${name} is not a valid number`);
+    return number;
+  }
+  const env = process.env[name];
+  return env ? stringVarToNumber(env, defaultVal) : defaultVal;
+}
+
+export const booleanVar = (name: string, defaultVal?: boolean): boolean => {
+  if (defaultVal === undefined) {
+    const stringVar = requireVar(name);
+    if (stringVar !== 'FALSE' && stringVar !== 'TRUE') {
+      throw new Error(`Environment variable ${name} should be either TRUE or FALSE`);
+    }
+    return stringVar === 'TRUE';
+  }
+  const env = process.env[name];
+  return defaultVal === true ? env === 'FALSE' : defaultVal === false;
+}
